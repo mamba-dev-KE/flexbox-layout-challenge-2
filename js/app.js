de194da26c0f597b9af0342abfebf38819ac396d
mamba-dev-KE <josephmaramba50@gmail.com>
2021-07-04 20:42:46 +0300

    work on navigation

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const toggleMenu = () => {
+    let x = document.getElementById("main-menu");
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
